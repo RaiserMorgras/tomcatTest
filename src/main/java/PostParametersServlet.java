243c73361ce7775ae4dfa4ec6f8adf720fa2228d
Arseniy Gromovoy <arseniygromovoy@gmail.com>
2020-03-05 17:15:40 +0200

    Test project for simple servlet.

---
+++ src/main/java/PostParametersServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+public class PostParametersServlet extends GenericServlet {
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        PrintWriter pw = servletResponse.getWriter();
+        Enumeration<String> enumeration = servletRequest.getParameterNames();
+        while(enumeration.hasMoreElements()) {
+            String pname = enumeration.nextElement();
+            pw.println(pname + " = " + servletRequest.getParameter(pname));
+        }
+        pw.close();
+    }
+}
